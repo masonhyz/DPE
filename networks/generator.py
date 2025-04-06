fc02f70578d888ccc8cc2625d381573e670fd1b4
masonhyz <mason.hu@mail.utoronto.ca>
2025-04-06 13:58:53 -0400

    big commit

--- networks/generator.py
+++ networks/generator.py
@@ -94,9 +94,7 @@ class Generator(nn.Module):
         directions_S = self.dir(alpha_S)
         directions_expD = self.mlp_exp(directions_D)
         directions_expS = self.mlp_exp(directions_S)
-        print(directions_expD.shape, directions_expS.shape)
         cos_sim = cosine_similarity(directions_expD.cpu().numpy(), directions_expS.cpu().numpy())
-        print("cos sim:", cos_sim)
         return cos_sim.item()
 
     def forward(self, img_source, img_drive,stage='train'):
--- run_demo_arcface.py
+++ run_demo_arcface.py
@@ -14,6 +14,7 @@ from insightface_backbone_conv import iresnet100
 from sklearn.metrics.pairwise import cosine_similarity
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import pandas as pd
 
 
 def load_image(filename, size):
@@ -101,21 +102,10 @@ class Demo(nn.Module):
             self.source.append(img_preprocessing(img,256).cuda())
 
 
-    def run(self, i):
+    def run(self):
         # choose a random frame from source video as source img and expression
         self.source_img = random.choice(self.source)
         self.exp_img = random.choice(self.source)
-
-        # output_dir = self.save_path
-
-        # crop_vi = os.path.join(output_dir, 'edit.mp4')
-        # out_fake = cv2.VideoWriter(crop_vi, cv2.VideoWriter_fourcc(*'mp4v'), 25, (256,256))
-
-        # crop_vi = os.path.join(output_dir, 'source.mp4')
-        # out_source = cv2.VideoWriter(crop_vi, cv2.VideoWriter_fourcc(*'mp4v'), 25, (256,256))
-
-        # crop_vi = os.path.join(output_dir, 'exp.mp4')
-        # out_exp = cv2.VideoWriter(crop_vi, cv2.VideoWriter_fourcc(*'mp4v'), 25, (256,256))
 
         print('==> running')
         with torch.no_grad():
@@ -123,32 +113,33 @@ class Demo(nn.Module):
             source_img = self.source_img
 
             # get expression latents, make sure they differ a lot
-            cos_sim_scalar = self.gen.compare_expression_latents(source_img, exp_img)
-            if True:
-                # print("ignored frame pairs with similar expressions")
-                # return None, None, None, None
+            exp_sim = self.gen.compare_expression_latents(source_img, exp_img)
+            # if True:
+            #     fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+            #     fig.suptitle(f"Cosine Similarity: {cos_sim_scalar:.4f}", fontsize=16)
+            #     titles = ["Source Image", "Expression Image"]
+            #     source_img = source_img[:,:3,:,:].clone().cpu().float().detach().numpy()
+            #     source_img = (np.transpose(source_img, (0, 2, 3, 1)) + 1) / 2.0 * 255.0
+            #     source_img = source_img.astype(np.uint8)[0]
+            #     exp_img = exp_img[:,:3,:,:].clone().cpu().float().detach().numpy()
+            #     exp_img = (np.transpose(exp_img, (0, 2, 3, 1)) + 1) / 2.0 * 255.0
+            #     exp_img = exp_img.astype(np.uint8)[0]
+            #     images = [source_img, exp_img]
 
-                fig, axes = plt.subplots(1, 2, figsize=(8, 4))
-                fig.suptitle(f"Cosine Similarity: {cos_sim_scalar:.4f}", fontsize=16)
-                titles = ["Source Image", "Expression Image"]
-                source_img = source_img[:,:3,:,:].clone().cpu().float().detach().numpy()
-                source_img = (np.transpose(source_img, (0, 2, 3, 1)) + 1) / 2.0 * 255.0
-                source_img = source_img.astype(np.uint8)[0]
-                exp_img = exp_img[:,:3,:,:].clone().cpu().float().detach().numpy()
-                exp_img = (np.transpose(exp_img, (0, 2, 3, 1)) + 1) / 2.0 * 255.0
-                exp_img = exp_img.astype(np.uint8)[0]
-                images = [source_img, exp_img]
+            #     for ax, img, title in zip(axes, images, titles):
+            #         ax.imshow(img)
+            #         ax.set_title(title)
+            #         ax.axis("off")
 
-                for ax, img, title in zip(axes, images, titles):
-                    ax.imshow(img)
-                    ax.set_title(title)
-                    ax.axis("off")
+            #     plt.tight_layout()
+            #     plt.subplots_adjust(top=0.85)  # leave space for suptitle
+            #     plt.savefig(os.path.join(self.save_path, f"comparison_{i}.png"))
+            #     plt.close()
+            #     return 
 
-                plt.tight_layout()
-                plt.subplots_adjust(top=0.85)  # leave space for suptitle
-                plt.savefig(os.path.join(self.save_path, f"comparison_{i}.png"))
-                plt.close()
-                return 
+            if exp_sim > 0.97:
+                print(f"Ignored frame pairs with exp sim {exp_sim:.4f}")
+                return None, None, None, None, exp_sim
             
             # transfer expression
             output_dict = self.gen(source_img, exp_img, 'exp')
@@ -159,24 +150,14 @@ class Demo(nn.Module):
             fake = fake[:,:3,:,:].clone().cpu().float().detach().numpy()
             fake = (np.transpose(fake, (0, 2, 3, 1)) + 1) / 2.0 * 255.0
             fake = fake.astype(np.uint8)[0]
-            # fake = cv2.cvtColor(fake, cv2.COLOR_RGB2BGR)
-            # out_fake.write(fake)
 
             source_img = source_img[:,:3,:,:].clone().cpu().float().detach().numpy()
             source_img = (np.transpose(source_img, (0, 2, 3, 1)) + 1) / 2.0 * 255.0
             source_img = source_img.astype(np.uint8)[0]
-            # source_img = cv2.cvtColor(source_img, cv2.COLOR_RGB2BGR)
-            # out_source.write(source_img)
 
             exp_img = exp_img[:,:3,:,:].clone().cpu().float().detach().numpy()
             exp_img = (np.transpose(exp_img, (0, 2, 3, 1)) + 1) / 2.0 * 255.0
             exp_img = exp_img.astype(np.uint8)[0]
-            # exp_img = cv2.cvtColor(exp_img, cv2.COLOR_RGB2BGR)
-            # out_exp.write(exp_img)
-
-            # out_fake.release()
-            # out_source.release()
-            # out_exp.release()
 
         print("==> evaluating")
         with torch.no_grad():
@@ -193,36 +174,50 @@ class Demo(nn.Module):
             id_fake = np.transpose(id_fake.numpy()[0], (1,0))
             id_source = np.transpose(id_source.numpy()[0], (1,0))
             cos_sim_matrix = cosine_similarity(id_fake, id_source)
-            cos_sim_scalar = np.mean(np.diag(cos_sim_matrix))
-            print(cos_sim_scalar)
+            cos_sim = np.mean(np.diag(cos_sim_matrix))
+            print(cos_sim)
         
-        return source_img, exp_img, fake, cos_sim_scalar
+        return source_img, exp_img, fake, cos_sim, exp_sim
 
-    def plot_batch(self):
+    def run_batch(self):
+        exp_sim_list = []
+        cos_sim_list = []
         for i in tqdm(range(self.args.n_samples)):
-            source_img, exp_img, fake_img, cos_sim_scalar = self.run()
-            if not source_img:
+            source_img, exp_img, fake_img, cos_sim, exp_sim = self.run()
+            cos_sim_list.append(cos_sim)
+            exp_sim_list.append(exp_sim)
+            if not source_img: 
                 continue
 
             fig, axes = plt.subplots(1, 3, figsize=(12, 4))
-            fig.suptitle(f"Cosine Similarity: {cos_sim_scalar:.4f}", fontsize=16)
-
             titles = ["Source Image", "Expression Image", "Generated Image"]
             images = [source_img, exp_img, fake_img]
-
             for ax, img, title in zip(axes, images, titles):
                 ax.imshow(img)
                 ax.set_title(title)
                 ax.axis("off")
 
             plt.tight_layout()
-            plt.subplots_adjust(top=0.85)  # leave space for suptitle
+            # plt.subplots_adjust(top=0.80)
+            fig.text(0.5, 0.02, f"Identity Similarity: {cos_sim:.4f}, Expression Similarity: {exp_sim:.4f}", 
+                    ha='center', fontsize=10, color='gray')
             plt.savefig(os.path.join(self.save_path, f"comparison_{i}.png"))
             plt.close()
 
-    def run_batch(self):
-        for i in tqdm(range(args.n_samples)):
-            self.run(i)
+        summary = {
+            'Mean': [np.mean(exp_sim_list), np.mean(cos_sim_list)],
+            'Median': [np.median(exp_sim_list), np.median(cos_sim_list)],
+            'Std Dev': [np.std(exp_sim_list), np.std(cos_sim_list)],
+            'Max': [np.max(exp_sim_list), np.max(cos_sim_list)],
+            'Min': [np.min(exp_sim_list), np.min(cos_sim_list)],
+            'Count': [np.sum(~np.isnan(exp_sim_list)), np.sum(~np.isnan(cos_sim_list))]
+        }
+        df = pd.DataFrame(summary, index=["Expression Similarities", "Identity Similarities"])
+        print(df.round(4).to_markdown())
+
+    # def run_batch(self):
+    #     for i in tqdm(range(args.n_samples)):
+    #         self.run()
 
 
 if __name__ == '__main__':
