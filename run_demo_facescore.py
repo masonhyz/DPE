5a66bd730748aa67bacc497c7e6ecb7e408859e1
masonhyz <mason.hu@mail.utoronto.ca>
2025-04-11 12:04:05 -0400

    face :

--- run_demo_facescore.py
+++ run_demo_facescore.py
@@ -301,10 +301,13 @@ if __name__ == '__main__':
         demo.run_batch()
     else:
         # run full directory
-        dir_path = args.s_path
-        mp4_files = [f for f in os.listdir(dir_path) if f.endswith('.mp4')]
-        for mp4 in tqdm(mp4_files, desc="Directory"):
-            args.s_path = os.path.join(dir_path, mp4)
-            print(f"==> Running on {args.s_path}")
-            demo = Demo(args)
-            demo.run_batch()
+        try:
+            dir_path = args.s_path
+            mp4_files = [f for f in os.listdir(dir_path) if f.endswith('.mp4')]
+            for mp4 in tqdm(mp4_files, desc="Directory"):
+                args.s_path = os.path.join(dir_path, mp4)
+                print(f"==> Running on {args.s_path}")
+                demo = Demo(args)
+                demo.run_batch()
+        except:
+            print(f"{dir_path} failed.")
